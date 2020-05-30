469d720b5bab587e84343d80b1f6af4d70dfad7e
darii petru <darii.petru@gmail.com>
2020-05-30 10:34:41 +0300

    c

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const http = require('http');
 const { MemoryTest } = require('./src/index');
 
-const memory = new MemoryTest(50);
-memory.increase();
-memory.decrease();
+const memory = new MemoryTest(500);
+
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+setInterval(() => {
+  console.log('increased')
+  memory.increase();
+}, 500);
--- src/memory-test.js
+++ src/memory-test.js
@@ -3,23 +3,21 @@ const { Buffer, constants } = require('buffer');
 class MemoryTest {
   constructor(deltaMb) {
     this.data = {};
+    this.deltaMb = deltaMb;
     this.counter = 0;
     this.nrBufInstances = Math.ceil(deltaMb * 1000000000 / constants.MAX_LENGTH);
-    this.buffLength = constants.MAX_LENGTH;
+    this.buffLength = constants.MAX_STRING_LENGTH;
     this.increase();
 
-    console.log(this.nrBufInstances);
+    console.log(constants);
   }
 
   increase() {
     this.data[this.counter] = []
-    for (let i = 0; i < this.nrBufInstances; i++) {
-      console.log('increase', i)
-      this.data[this.counter].push(Buffer.alloc(this.buffLength))
+    for (let i = 0; i < this.deltaMb; i++) {
+      this.data[this.counter].push(' '.repeat(1e8));
     }
     this.counter++;
-
-    console.log('increase', constants)
   }
 
   decrease() {
