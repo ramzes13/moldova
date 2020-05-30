02fcc373838efe729662e351e3cf68a9b69ee2e2
darii petru <darii.petru@gmail.com>
2020-05-30 14:12:37 +0300

    c

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const http = require('http');
-const { MemoryTest } = require('./src/index');
+// const http = require('http');
+// const { MemoryTest } = require('./src/index');
 
-const memory = new MemoryTest(500);
+// const memory = new MemoryTest(500);
 
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+// http.createServer(function (req, res) {
+//   res.write('Hello World!'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(8080); //the server object listens on port 8080
 
-setInterval(() => {
-  console.log('increased')
-  memory.increase();
-}, 500);
+// setInterval(() => {
+//   console.log('increased')
+//   memory.increase();
+// }, 500);
